f16b25424cdc0fbe1e7d962a389c72baf74f5bfe
Rawley <75388349+rawleyfowler@users.noreply.github.com>
2023-06-19 07:40:29 -0600

    Update sleeper_test.go

--- sleeper_test.go
+++ sleeper_test.go
@@ -1,7 +1,7 @@
 package sleeper_test
 
 /*
-   Copyright 2023 Mollusc Labs Inc
+   Copyright 2023 Mollusc Labs
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
